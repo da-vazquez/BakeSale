cd3802a9a2c42fc902c49113df606724549622b6
Daniel V <71048973+da-vazquez@users.noreply.github.com>
2021-10-19 15:44:56 -0700

    React Native Project Week 2 (#1)
    * searchbar component to search articles based on keywords
    
    * component that shows details for item touched by user
    
    * converts price in cents to dollars
    
    * ajax reqs for fetching deals from bakesaleforgood
    
    * installed lodash.debounce
    
    * week 2 functionality / styling
    
    * deal container component created
    
    * items displayed using flatlist

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ajax from './src/ajax';
+import DealList from './src/components/DealList';
+import DealDetail from './src/components/DealDetail';
+import SearchBar from './src/components/SearchBar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    deals: [],
+    dealsFromSearch: [],
+    currentDealId: null,
+  };
+
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    this.setState({ deals })
+  };
+
+  searchDeals = async (searchTerm) => {
+    const dealsFromSearch = await ajax.fetchDealSearchResults(searchTerm)
+    this.setState({ dealsFromSearch });
+  };
+
+  setCurrentDeal = (dealId) => {
+    this.setState({
+      currentDealId: dealId
+  })
+};
+
+  clearSearch = () => {
+    this.setState({ dealsFromSearch: [] });
+  }
+
+  unsetCurrentDeal = () => {
+    this.setState({
+      currentDealId: null
+  })
+};
+
+  currentDeal = () => {
+    return this.state.deals.find(
+      (deal) => deal.key === this.state.currentDealId
+    )
+  };
+
+  render() {
+    if (this.state.currentDealId) {
+      return (
+      <View style={styles.main}>
+        <DealDetail 
+          initialDealData={this.currentDeal()}
+          onBack={this.unsetCurrentDeal} />
+      </View>
+      )
+    }
+    const dealsToDisplay = this.state.dealsFromSearch.length > 0 ? 
+      this.state.dealsFromSearch : this.state.deals 
+    
+    if (dealsToDisplay.length > 0) {
+      return (
+      <View style={styles.main}>
+        <SearchBar searchDeals={this.searchDeals}/>
+        <DealList deals={dealsToDisplay} onItemPress={this.setCurrentDeal}/>
+      </View>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>BakeSale</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1, 
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  main: {
+    marginTop: 30
+  },
+  header: {
+    fontSize: 40
+  }
+})
+
+
+
+export default App;
